96e09917ebd3956846d6b3e32769a7305e49c556
jforbes <jforbes@brightcove.com>
2016-12-05 13:32:13 -0500

    Fix tests

--- test/videojs-flashls-source-handler.test.js
+++ test/videojs-flashls-source-handler.test.js
@@ -10,7 +10,7 @@ QUnit.test('the environment is sane', function(assert) {
   assert.strictEqual(typeof Array.isArray, 'function', 'es5 exists');
   assert.strictEqual(typeof sinon, 'object', 'sinon exists');
   assert.strictEqual(typeof videojs, 'function', 'videojs exists');
-  assert.strictEqual(typeof handler, 'function', 'handler is a function');
+  assert.strictEqual(typeof handler, 'object', 'handler is a function');
 });
 
 QUnit.module('videojs-flashls-source-handler', {
@@ -34,3 +34,4 @@ QUnit.module('videojs-flashls-source-handler', {
     this.clock.restore();
   }
 });
+
