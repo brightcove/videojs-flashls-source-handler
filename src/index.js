aeb6c4b3d3291628b36774a94475c3fbd2809dbf
Matthew Neil <mneil@mneil-retina-2016.vidmark.local>
2016-12-06 17:12:38 -0500

    add id3 cues to metadata track

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import videojs from 'video.js';
+import window from 'global/window';
+
+const parseSyncSafeInteger = function(data) {
+  return (data.charCodeAt(0) << 21) |
+          (data.charCodeAt(1) << 14) |
+          (data.charCodeAt(2) << 7) |
+          (data.charCodeAt(3));
+};
 
 /*
  * Registers the SWF as a handler for HLS video.
@@ -56,6 +64,60 @@ FlashlsSourceHandler.canHandleSource = function(source, options) {
  */
 FlashlsSourceHandler.handleSource = function(source, tech, options) {
   tech.setSrc(source.src);
+
+  tech.on('id3updated', (event, data) => {
+    const id3tag = window.atob(data[0]);
+    let frameStart = 10;
+
+    if (id3tag.charCodeAt(5) & 0x40) {
+      // advance frame start past extended header
+      frameStart += 4;
+      frameStart += parseSyncSafeInteger(id3tag.substring(10, 14));
+    }
+
+    const frameSize = parseSyncSafeInteger(id3tag.substring(frameStart + 4, frameStart + 8));
+
+    if (this.metadataTrack_) {
+      const Cue = window.WebKitDataCue || window.VTTCue;
+      const time = tech.currentTime();
+      const cue = new Cue(time,
+                          time,
+                          id3tag.substring(frameStart + 10, frameStart + frameSize + 10));
+
+      this.metadataTrack_.addCue(cue);
+
+      if (this.metadataTrack_.cues && this.metadataTrack_.cues.length) {
+        const cues = this.metadataTrack_.cues;
+        const cuesArray = [];
+        let duration = tech.duration();
+
+        if (isNaN(duration) || Math.abs(duration) === Infinity) {
+          duration = Number.MAX_VALUE;
+        }
+
+        for (let i = 0; i < cues.length; i++) {
+          cuesArray.push(cues[i]);
+        }
+
+        cuesArray.sort((a, b) => a.startTime - b.startTime);
+
+        for (let i = 0; i < cuesArray.length - 1; i++) {
+          if (cuesArray[i].endTime !== cuesArray[i + 1].startTime) {
+            cuesArray[i].endTime = cuesArray[i + 1].startTime;
+          }
+        }
+        cuesArray[cuesArray.length - 1].endTime = duration;
+      }
+    }
+  });
+
+  if (tech.options_ && tech.options_.playerId) {
+    const _player = videojs(tech.options_.playerId);
+
+    _player.ready(() => {
+      this.metadataTrack_ = _player.addTextTrack('metadata', 'Timed Metadata')
+    });
+  }
 };
 
 /**
