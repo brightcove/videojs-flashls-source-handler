fb2b9115f5cc372b50090c0efd740c01fafcea0c
shahlabs <lshah@brightcove.com>
2017-11-09 17:30:30 -0500

    Updated test

--- test/playlists.test.js
+++ test/playlists.test.js
@@ -6,41 +6,14 @@ QUnit.module('Flashls playlists');
 
 QUnit.test('triggers an event when the active media changes', function(assert) {
   let mediaChange = 0;
-  const levels = [
-    {
-      index: 0,
-      width: 640,
-      height: 360,
-      bitrate: 865000,
-      url: 'playlist-0-uri'
-    },
-    {
-      index: 1,
-      width: 1280,
-      height: 720,
-      bitrate: 12140000,
-      url: 'playlist-1-uri'
-    },
-    {
-      index: 2,
-      width: 1920,
-      height: 1080,
-      bitrate: 16120000,
-      url: 'playlist-2-uri'
-    }
-  ];
 
-  let currentLevel = -1;
-
-  const tech = makeMochTech({ levels: () => levels, level: () => currentLevel},
-    {level: (val) => currentLevel = val});
+  const tech = makeMochTech({});
 
   /* eslint-disable no-unused-vars */
   // need to create handler to setup event listeners
   const handler = new FlashlsHandler('this.m3u8', tech, {});
   /* eslint-enable no-unused-vars */
 
-  currentLevel = 0;
   tech.trigger('levelswitch');
 
   tech.on('mediachange', () => {
@@ -49,7 +22,6 @@ QUnit.test('triggers an event when the active media changes', function(assert) {
 
   assert.equal(mediaChange, 0, 'Initial selection is not a media changing');
 
-  currentLevel = 1;
   tech.trigger('levelswitch');
   assert.equal(mediaChange, 1, 'fired a mediachange');
 });
@@ -81,13 +53,11 @@ QUnit.test('mediachange changes playlist.media function', function(assert) {
 
   let currentLevel = -1;
 
-  const tech = makeMochTech({ levels: () => levels, level: () => currentLevel},
-    {level: (val) => currentLevel = val});
+  const tech = makeMochTech({ levels: () => levels, level: () => currentLevel}, {});
 
   const handler = new FlashlsHandler('this.m3u8', tech, {});
 
   currentLevel = 0;
-  tech.trigger('levelswitch');
   let media = handler.playlists.media();
 
   assert.equal(media.resolvedUri, 'playlist-0-uri', 'correct resolvedUri');
@@ -96,7 +66,7 @@ QUnit.test('mediachange changes playlist.media function', function(assert) {
   assert.equal(media.attributes.RESOLUTION.height, 360, 'correct height');
 
   currentLevel = 1;
-  tech.trigger('levelswitch');
+
   media = handler.playlists.media();
 
   assert.equal(media.resolvedUri, 'playlist-1-uri', 'correct resolvedUri');
